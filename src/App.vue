f61e5c06e9bb135f1c69149529c4755ff9978bbb
andreiz53 <andrei.zamfira53@gmail.com>
2023-06-13 12:47:32 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div id="page-container">
+    <div class="main-section">
+      <div class="main-row">
+        <div class="left-col">
+          <div class="card front"></div>
+          <div class="card back"></div>
+        </div>
+        <div class="right-col"></div>
+      </div>
+    </div>
+  </div>
+</template>
